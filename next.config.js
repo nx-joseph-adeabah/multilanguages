8a1f43e166ba13b11e2d9af6a5ade5eb2e73cef9
nx-joseph-adeabah <joseph.adeabah@nexum.com>
2023-03-13 10:03:39 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,22 +3,22 @@ module.exports = {
 
   images: {},
   i18n: {
-    locales: ["en", "de", "fr", "en-gb"],
-    defaultLocale: "fr",
-    // localeDetection: true,
+    locales: ['en', 'de', 'fr', 'en-gb'],
+    defaultLocale: 'fr',
+    localeDetection: true,
     domains: [
       {
-        domain: "https://multilanguages-blond.vercel.app",
-        // defaultLocale: "en",
-        locales: ["en-gb","en"]
+        domain: 'https://multilanguages-blond.vercel.app',
+        defaultLocale: 'en',
+        locales:['en-gb']
       },
       {
-        domain: "https://multilanguages-de.vercel.app/",
-        defaultLocale: "de",
+        domain: 'https://multilanguages-de.vercel.app/',
+        defaultLocale: 'de',
       },
       {
-        domain: "https://multilanguages-fr.vercel.app/",
-        defaultLocale: "fr",
+        domain: 'https://multilanguages-fr.vercel.app/',
+        defaultLocale: 'fr',
       },
     ],
   },
