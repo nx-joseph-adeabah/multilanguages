36dc3c9f8fe9da53cc7ef8d11be54bec2be24489
nx-joseph-adeabah <joseph.adeabah@nexum.com>
2023-03-12 17:36:09 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar({ blok, locale, locales, defaultLocale }) {
         router.push(`${domain.domain}/${loc}`);
       }
       if(domain.defaultLocale === 'en'){
-        router.push(`${router.domainLocales[0].domain}`);
+        router.push(`${router.domainLocales[0].domain}/${loc}`);
       }
 
       if(domain.defaultLocale === 'en-gb'){
