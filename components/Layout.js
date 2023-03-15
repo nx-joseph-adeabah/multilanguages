28f222c7eb60ccbe5fad1ed3408933bfe01ccd4c
nx-joseph-adeabah <joseph.adeabah@nexum.com>
2023-03-15 21:42:22 +0000

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,6 @@
-import Navbar from "./Navbar";
 export default function Layout({ children, locale, locales, defaultLocale }) {
   return (
     <div className="">
-      <Navbar/>
       {children}
       <p>{locale}</p>
     </div>
