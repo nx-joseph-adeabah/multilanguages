0298f6273178495a42eb3b5d8c20bb3ccdc4ec1b
nx-joseph-adeabah <joseph.adeabah@nexum.com>
2023-03-16 00:56:14 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import StoryblokClient from "storyblok-js-client";
 import styles from "../styles/Home.module.css";
 
 import {
@@ -9,16 +8,11 @@ import {
 } from "@storyblok/react";
 import Layout from "../components/Layout";
 
-export default function Home({ story, data, locale, locales, defaultLocale }) {
-  
+export default function Home({ story, locale, locales, defaultLocale }) {
   story = useStoryblokState(story, {
-     customParent: "http://localhost:3000/",
+    customParent: "http://localhost:3000/",
     language: locale,
   });
-  
-  console.log("data", data)
-
-
 
   return (
     <div>
@@ -41,17 +35,6 @@ export default function Home({ story, data, locale, locales, defaultLocale }) {
 }
 
 export async function getStaticProps({ locale, locales, defaultLocale }) {
-
-  const Storyblok = new StoryblokClient({
-    oauthToken: "JSy2OVQ3xAKyCANBTC7foAtt-142837-fc7G7oFzxC8bp7qan7Z3",
-  });
-   
-
-   const res = await Storyblok.get("/spaces/204132/stories/");
-    
-    let folda = res.data.stories.map((dat)=>{
-      return dat.slug;
-    })
   let slug = "home";
   let loc = locale;
 
@@ -60,18 +43,14 @@ export async function getStaticProps({ locale, locales, defaultLocale }) {
     language: locale,
   };
 
- 
-
-   const storyblokApi = getStoryblokApi();
+  const storyblokApi = getStoryblokApi();
   let { data } = await storyblokApi.get(`cdn/stories/${loc}/${slug}`, sbParams);
 
   return {
     props: {
       defaultLocale,
       story: data ? data.story : false,
-       data: res ? res.data : false,
       key: data ? data.story.id : false,
-      // f: folda,
       locale,
       locales,
     },
